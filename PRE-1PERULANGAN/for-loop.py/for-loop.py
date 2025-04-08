617271d57317a127cdb562a4f45bedd63a366ef7
Sandikaakbar <sandikaakbar2802@gmail.com>
2025-04-08 17:28:41 +0800

    Add files via upload

---
+++ PRE-1PERULANGAN/for-loop.py/for-loop.py
@@ -0,0 +1,18 @@
+#perulangan (loop)
+#for kondisi : jika kondisi terpenuhi
+#   aksi jalankan aksi
+
+angka_list = [0,2,4,6,8,10] # ini adalah list
+print(angka_list)
+
+#angka = 0
+#print(angka)
+#angka == 1 = angka = angka 1
+#print (angka)
+#angka == 1
+#print(angka)
+#for loop
+for i in angka_list :
+    print(f" i yang sekarang -> (i)" )
+    
+print("loop berakhir/n")
---
+++ PRE-1PERULANGAN/while-loop.py
@@ -0,0 +1,9 @@
+Angka = 0 
+while Angka < 3:
+    print("*" * Angka)
+    Angka += 1
+
+Angka = 0 
+while Angka < 3:
+    Angka += 1
+    print("*" * Angka)
