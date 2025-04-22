df09ae84398520164113ce5c93e23b7f3dae6d3d
Sandikaakbar <sandikaakbar2802@gmail.com>
2025-04-22 14:00:55 +0800

    Add files via upload

---
+++ PRE-1PERULANGAN/function.py
@@ -0,0 +1,86 @@
+def sapa():
+    print("Halo, selamat datang!")
+
+sapa()  # Memanggil fungsi
+sapa()
+sapa()
+
+
+# function dengan 1 parameter
+def sapa_nama(nama):
+    print(f"Halo, {nama}!")
+
+# Pemanggilan Function
+sapa_nama("Adam")
+
+
+# function dengan lebih dari 1 parameter
+def luas_segitiga(alas, tinggi):
+    luas = (alas * tinggi) / 2
+    print("Luas segitiga: %f" % luas)
+
+# Pemanggilan Function
+luas_segitiga(4, 6)
+
+
+def tambah(a, b):
+    return a + b
+
+# pemanggilan fungsi
+hasil = tambah(3, 5)
+print("Hasil:", hasil)
+
+
+# rumus sisi x sisi
+def luas_persegi(sisi):
+    luas = sisi * sisi
+    return luas
+
+
+# rumus: sisi x sisi x sisi
+def volume_persegi(sisi):
+    volume = luas_persegi(sisi) * sisi
+    return volume
+
+# pemanggilan fungsi
+vol_persegi = volume_persegi(6)
+print(f"Volume Persegi = {vol_persegi}")
+      
+      
+def student(firstname, lastname ='Mark', standard ='Fifth'):
+    print(firstname, lastname, 'studies in', standard, 'Standard')
+
+#pemanggilan function, dengan 1 argument
+# pemanggilan function, dengan 1 argumen
+student("Wallberg")
+
+# pemanggilan function, dengan 3 argumen
+student('John', 'Gates', 'Seventh')  
+
+# pemanggilan function dengan 2 argumen
+student('John', 'Gates')               
+student('John', 'Seventh')
+
+
+def kali(a: int, b: int) -> int:
+    return a * b
+
+# Pemanggilan function
+print("Hasil = " ,kali(3, 4))
+print("Tipe Data : ", type(kali(3,4)))
+
+
+# *args untuk argumen bervariasi
+def myFun(*argv):
+    for arg in argv:
+        print(arg)
+
+myFun('Hello', 'Selamat Datang', 'Di', 'PTI UNDIKMA')
+
+
+def info_mahasiswa(**data):
+    for key, value in data.items():
+        print(f"{key}: {value}")
+
+# Pemanggilan function
+info_mahasiswa(nama="Rina", jurusan="TI", angkatan=2022)
